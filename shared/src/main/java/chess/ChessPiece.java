17ba8aff4dce63a1324616e29ab12d0bdf22e666
suorce <lukesuorsa@gmail.com>
2026-09-08 15:06:05 -0600

    start implementation of pieceMoves()

--- shared/src/main/java/chess/ChessPiece.java
+++ shared/src/main/java/chess/ChessPiece.java
@@ -48,6 +48,10 @@ public class ChessPiece {
      * @return Collection of valid moves
      */
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+        if (piece.getPieceType() == PieceType.BISHOP) {
+            return List.of(new ChessMove(new ChessPosition(5,4), new ChessPosition(1,8), null));
+        }
         return List.of();
     }
 }
